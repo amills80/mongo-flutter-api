da4c6d0370673f72f43ffc82f2a385cca64ee01e
Alan Mills <alan.mills@beeline.com>
2019-09-25 13:09:12 -0400

    Add heroku-mLabs url to project.

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var express = require('express'),
 var mongoose = require('mongoose')
 Task = require('./models/todoListModel'), //created model loading here
 bodyParser = require('body-parser');
+// uriString = 'mongodb://amills:pass1234@cluster-shard-00-00-6ezzw.azure.mongodb.net:27017,cluster-shard-00-01-6ezzw.azure.mongodb.net:27017,cluster-shard-00-02-6ezzw.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster-shard-0&authSource=admin&retryWrites=true&w=majority';
+uriString = 'mongodb://test_db:Dem01234@ds127362.mlab.com:27362/heroku_gmtb03l5'
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://amills:pass1234@cluster-shard-00-00-6ezzw.azure.mongodb.net:27017,cluster-shard-00-01-6ezzw.azure.mongodb.net:27017,cluster-shard-00-02-6ezzw.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(uriString, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 }); 
